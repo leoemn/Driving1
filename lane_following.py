096dbb52aa0d471ff688aef1fb9933ae1c9b4a23
leoemn <sagarjoshi1013@gmail.com>
2023-11-29 13:33:57 +0000

    coonect to server access blueprint & world spawn a vehicle create camera and attach it to vehicle

--- lane_following.py
+++ lane_following.py
@@ -12,6 +12,7 @@ world = client.get_world()
 blueprint_library = world.get_blueprint_library()
 
 #spawn a vehicle 
+
 #Choose a vehicle blueprint
 vehicle_bp = blueprint_library.find('vehicle.tesla.model3')
 
@@ -22,6 +23,7 @@ spawn_point = world.get._map.get_spawn_points()[0]
 vehicle = world.spawn_actor(vehicle_bp, spawn_point)
 
 #create a camera sensor and attach it to the vehicle
+
 camera_bp = blueprint_library.find('sensor.camera.rgb')
 camera_transform = carla.Transform(carla.Location(x=1.5, z=2.4))
 camera = world.spawn_actor(camera_bp, camera_transform, attach_to = vehicle)
